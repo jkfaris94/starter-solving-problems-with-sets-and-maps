abf31fd17885b384ca165c19f86f3c1ee6c9c7ad
Johnny <jkfaris@gmail.com>
2025-07-18 17:40:08 -0700

    added solution for intersection. tests passing

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,16 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+    const interMap = new Map();
+    const newSet = new Set();
+
+    for (const item of a) {
+        interMap.set(item, true);
+    }   
+    for (const item of b) {
+        if (interMap.has(item)) {
+            newSet.add(item);
+        }
+    }
+    return Array.from(newSet);
+}
 
 module.exports = intersection;
